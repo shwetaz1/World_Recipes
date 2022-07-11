ccb2bf7a13b7b2dbd19fed3e01e5fd12974d7c9a
shwetaz1 <93508288+shwetaz1@users.noreply.github.com>
2022-07-11 23:43:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDIwSwi2szr6UU2MF1xzl3Vx54KbSzM3pc",
+authDomain: "world-recipes-e907c.firebaseapp.com",
+projectId: "world-recipes-e907c",
+storageBucket: "world-recipes-e907c.appspot.com",
+messagingSenderId: "539325790383",
+appId: "1:539325790383:web:f7727dbb1f49bee4f43c5a",
+measurementId: "G-7PK3YYLGX2"
+};
+const body = document.querySelector("body"),
+      nav = document.querySelector("nav"),
+      modeToggle = document.querySelector(".dark-light"),
+      searchToggle = document.querySelector(".searchToggle"),
+      sidebarOpen = document.querySelector(".sidebarOpen"),
+      siderbarClose = document.querySelector(".siderbarClose");
+
+      let getMode = localStorage.getItem("mode");
+          if(getMode && getMode === "dark-mode"){
+            body.classList.add("dark");
+          }
+
+// js code to toggle dark and light mode
+      modeToggle.addEventListener("click" , () =>{
+        modeToggle.classList.toggle("active");
+        body.classList.toggle("dark");
+
+        // js code to keep user selected mode even page refresh or file reopen
+        if(!body.classList.contains("dark")){
+            localStorage.setItem("mode" , "light-mode");
+        }else{
+            localStorage.setItem("mode" , "dark-mode");
+        }
+      });
+
+// js code to toggle search box
+        searchToggle.addEventListener("click" , () =>{
+        searchToggle.classList.toggle("active");
+      });
+ 
+      
+//   js code to toggle sidebar
+sidebarOpen.addEventListener("click" , () =>{
+    nav.classList.add("active");
+});
+
+body.addEventListener("click" , e =>{
+    let clickedElm = e.target;
+
+    if(!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")){
+        nav.classList.remove("active");
+    }
+});
+
+
